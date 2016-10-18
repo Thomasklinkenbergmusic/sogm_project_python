61980fd275b44b109fa42cdca1e39c305280a03c
Thomasklinkenbergmusic <thomasklinkenbergmusic@gmail.com>
2016-10-18 12:39:01 +0200

    Worked on algorithm.py and intergrated midi into the algorithm.py. Probably gonna work in another branches because this isn't testing anymore...

--- miditest.py
+++ miditest.py
@@ -12,16 +12,15 @@ pygame.midi.init()
 player = pygame.midi.Output(1, 0)
 player.set_instrument(0,1)
 pygame.midi.MidiException(1)
-list=[[60, 100], [63, 100], [67, 100], [60, 100]]
-
+list=[[1, 96], [0, 0], [1, 95], [1, 34], [1, 92], [0, 0], [1, 29], [0, 0], [1, 94], [0, 0], [0, 0], [1, 117], [0, 0], [0, 0], [1, 53], [0, 0]]
 # player.note_on(60, 100)
 # time.sleep(1)
 # player.note_off(60, 100)
 
-for x in range(0, 4) :
+for x in range(0, 2) :
     for x in range(0, len(list)) :
         player.note_on(list[x][0], list[x][1])
-        time.sleep(0.25)
+        time.sleep(0.1)
         player.note_off(list[x][0], list[x][1])
         time.sleep(0.1)
 
--- scalingtest.py
+++ scalingtest.py
@@ -2,14 +2,34 @@ import random
 
 """WERKT!"""
 
-AMOUNT = 80
+AMOUNT = 50
 
-def p(x) :
+kick=[]
+for I in range(0,8) :
+    kick.append([0,0])
+
+def percentage(x) :
     result = 0.0004 * x
     return result
+
 def R100() :
     x =  random.randint(1, 100)
     return x
+
+def Rvel(x, y) :
+    if y < 0 :
+        Y = 0
+    if y > 1:
+        Y = 1
+    min = int(x * (1 - y))
+    max = int(x * (1 + y))
+    if min < 1 :
+        min = 1
+    if max > 127 :
+        max = 127
+    result = random.randint(min, max)
+    return result
+
 def scale(x, oMin, oMax, nMin, nMax):
     if oMin == oMax:
         return None
@@ -37,6 +57,7 @@ def scale(x, oMin, oMax, nMin, nMax):
         result = newMax - portion
 
     return result
+
 def chance(x, y) :
     if x <= 50 :
         resultx = scale(x, 0, 100, 100, 0)
@@ -49,7 +70,19 @@ def chance(x, y) :
         result = 100
     return result
 
-print chance(AMOUNT, p(20))
-print chance(AMOUNT, p(20))
-print chance(AMOUNT, p(30))
-print chance(AMOUNT, p(10))
+def notechance(x, p1, y, p2) :
+    list = [0, 0]
+    if R100() < chance(x, p1) :
+        list = [1, Rvel(y, p2)]
+    return list
+
+kick[0]=notechance(AMOUNT, percentage(100), 100, 0.1)
+kick[1]=notechance(AMOUNT, percentage(20), 30, 0.5)
+kick[2]=notechance(AMOUNT, percentage(40), 50, 0.1)
+kick[3]=notechance(AMOUNT, percentage(60), 80, 0.25)
+kick[4]=notechance(AMOUNT, percentage(70), 100, 0.1)
+kick[5]=notechance(AMOUNT, percentage(10), 30, 0.2)
+kick[6]=notechance(AMOUNT, percentage(50), 20, 0.5)
+kick[7]=notechance(AMOUNT, percentage(40), 60, 0.2)
+
+print kick
